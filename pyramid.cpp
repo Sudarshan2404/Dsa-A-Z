9510eceae7572a2af4506033ba10dabee0c0cb76
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-24 22:32:47 +0530

    .

--- pyramid.cpp
+++ pyramid.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 
-int main() {
-  for(int i=0; i<5; i++){
-    for(int j=0; j<5-i-1; j++){
-      cout<<" ";
-    }
-    for(int j=0; j<2*i+1; j++){
-      cout<<"*";
-    }
-    cout<<endl;
-
+int main(){
+  int n = 6;
+for(int i=0; i<n; i++){
+  for(int j=0; j<n-i-1; j++){
+    cout<<" ";
   }
+  for(int j=0; j<2*i+1; j++){
+    cout<<"*";
+  }
+  cout<<endl;
+}
   return 0;
 }
