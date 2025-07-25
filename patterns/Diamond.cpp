74418615a2fa299d616104e781d1dd415081abcd
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-25 21:29:10 +0530

    diamond done

--- patterns/Diamond.cpp
+++ patterns/Diamond.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main(){
-  
+  int n=8;
+  for(int i=0; i<n/2; i++){
+    for(int j=0; j<n-i-1; j++){
+      cout<<" ";
+    }
+    for(int j=0; j<2*i+1; j++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+  for(int i=(n/2)-2; i>=0; i--){
+    for(int j=0; j<n-i-1; j++){
+      cout<<" ";
+    }
+    for(int j=0; j<2*i+1; j++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
   return 0;
 }
