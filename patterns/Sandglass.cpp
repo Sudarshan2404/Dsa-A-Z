d1a58bc2315af6553b83084eb839d9ab9140bbf3
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-07-25 21:40:47 +0530

    sadglass star pattern

--- patterns/Sandglass.cpp
+++ patterns/Sandglass.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main(){
-  int n=6;
-  for(int i=0; i<n/2; i++){
+  int n=10;
+  for(int i=n/2; i>0; i--){
     for(int j=0; j<n-i-1; j++){
       cout<<" ";
     }
-    for(int j=0; j<2*i+1; j++){
+     for(int j=0; j<2*i-1; j++){
       cout<<"*";
     }
     cout<<endl;
   }
-  for(int i=(n/2)-2; i>=0; i--){
+  for(int i=2; i<=n/2; i++){
     for(int j=0; j<n-i-1; j++){
       cout<<" ";
     }
-    for(int j=0; j<2*i+1; j++){
+     for(int j=0; j<2*i-1; j++){
       cout<<"*";
     }
     cout<<endl;
