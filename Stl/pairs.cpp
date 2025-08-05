858b78b7a376a5c9a775421e4d37c6eb04b39828
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-08-05 22:12:41 +0530

    stl

---
+++ Stl/pairs.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <utility>
+#include <iostream>
+using namespace std;
+
+int main(){
+  pair<int, int> p = {1,2};
+  cout<<p.first<<" "<<p.second;
+  return 0;
+}
---
+++ patterns/Hourglass.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n=10;
+  for(int i=n/2; i>0; i--){
+    for(int j=0; j<n-i-1; j++){
+      cout<<" ";
+    }
+     for(int j=0; j<2*i-1; j++){
+      cout<<j;
+    }
+    cout<<endl;
+  }
+  for(int i=2; i<=n/2; i++){
+    for(int j=0; j<n-i-1; j++){
+      cout<<" ";
+    }
+     for(int j=0; j<2*i-1; j++){
+      cout<<j;
+    }
+    cout<<endl;
+  }
+  return 0;
+}
