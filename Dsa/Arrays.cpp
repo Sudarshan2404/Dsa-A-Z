5ec38706082e0c23ec71ac7b7d5b309c1b1681b2
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-10-03 23:27:03 +0530

    is sorted?

--- Dsa/Arrays.cpp
+++ Dsa/Arrays.cpp
@@ -3,18 +3,38 @@
 #include <climits>
 using namespace std;
 
-
-
-int main(){
-  vector<int> nums = {15,38,97,58};
+int main()
+{
+  vector<int> nums = {15, 38, 58, 97, 97, 98};
 
   int max = INT_MIN;
-  for(int i=0; i<nums.size(); i++){
-    if(nums[i] > max){
+  for (int i = 0; i <= nums.size(); i++)
+  {
+    if (nums[i] > max)
+    {
       max = nums[i];
     }
   }
 
-  cout<<"The Largest number in vector is "<<max;
+  cout << "The Largest number in vector is " << max << endl;
+
+  // Check if an array is sorted or not
+  int sorted = true;
+  for (int i = 0; i < nums.size() - 1; i++)
+  {
+    if (nums[i] > nums[i + 1])
+    {
+      sorted = false;
+    }
+  }
+
+  if (sorted)
+  {
+    cout << "Array is sorted";
+  }
+  else
+  {
+    cout << "array is not sorted";
+  }
   return 0;
 }
