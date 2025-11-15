46e0c4ecd45014c7fa8098e47629255ddd72a820
Sudarshan Kulkarni <Sudarshan.kul2404@gmail.com>
2025-11-15 14:26:21 +0530

    LEarned stl

--- Stl/pairs.cpp
+++ Stl/pairs.cpp
@@ -1,10 +1,20 @@
 #include <stdlib.h>
-#include <utility>
 #include <iostream>
 using namespace std;
 
 int main(){
-  pair<int, int> p = {1,2};
-  cout<<p.first<<" "<<p.second;
+  // Normal pair
+  pair <int, int> p = {1,2};
+  cout << p.first << endl;
+  cout << p.second << endl;
+
+  // pair inside pair
+  pair <int, pair<int, int>> p2 = {1,{1,16}};
+  cout<<p2.second.second<<endl;
+
+
+  // Array in a pair
+  pair <int, int> arr[] = {{8,17}, {2,8}, {3,7}};
+  cout<<arr[1].second;
   return 0;
 }
